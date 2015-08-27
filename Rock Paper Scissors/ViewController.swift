487acfc9b69714165dce52dbc304132a0b4575fb
eleanorstrib <eleanor@sloan.mit.edu>
2015-08-26 20:30:45 -0700

    initial set up of game

---
+++ Rock Paper Scissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by Eleanor Stribling on 8/26/15.
+//  Copyright (c) 2015 eleanorstrib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Rock Paper Scissors/resultViewController.swift
@@ -0,0 +1,9 @@
+//
+//  resultViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by Eleanor Stribling on 8/26/15.
+//  Copyright (c) 2015 eleanorstrib. All rights reserved.
+//
+
+import Foundation
