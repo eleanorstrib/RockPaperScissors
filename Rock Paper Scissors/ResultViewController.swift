28bd43a6f7bb2fc31a057031c5753df2b5c1517c
eleanorstrib <eleanor@sloan.mit.edu>
2015-09-01 00:23:21 -0700

    problem with previous resultViewController -- removed and rebuilding it

---
+++ Rock Paper Scissors/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by Eleanor Stribling on 9/1/15.
+//  Copyright (c) 2015 eleanorstrib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultViewController: UIViewController {
+    var computerChoice = UInt32()
+    var playerChoice = UInt32()
+    
+    
+
+}
+
