d8d880e8f3e14f68a3aeccf5f9e9b8c7b244fc50
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-06 17:10:11 -0700

    have the table working with title and subtitle placeholders, but it's still having trouble reading the array with results to populate

--- PlayerHistoryViewController.swift
+++ PlayerHistoryViewController.swift
@@ -9,10 +9,17 @@
 import Foundation
 import UIKit
 
-class PlayerHistoryViewController: UIViewController, UITableViewDelegate {
+class PlayerHistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var playerHistoryTable: UITableView!
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
     // import the history array from the AppDelegate
     var history: [History] {
@@ -20,16 +27,23 @@ class PlayerHistoryViewController: UIViewController, UITableViewDelegate {
     }
     
     // find out the number of rows/items in history and pass to the view
-    func TableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        println(history.count)
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        println(history)
         return history.count
     }
     
-//    override func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-////        let cell = tableView.dequeueReusableHeaderFooterViewWithIdentifier("gameResult") as! UITableViewCell
-////        return cell
-//    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("gameResult") as! UITableViewCell
+        let game = history[indexPath.row]
+        
+//        // Set the name and image
+//        cell.textLabel?.text = history.result
+//        cell.imageView?.image = UIImage(named: history.graphic)
+//        cell.detailTextLabel?.text = "You chose \(history.player) & the computer chose \(history.computer)"
 
+        return cell
+
+    }
     
     
 }
